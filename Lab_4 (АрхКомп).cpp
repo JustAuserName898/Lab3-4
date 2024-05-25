f3a35b14ba63a2e740dad5fbb7fdf3e34d7b96c4
JustAuserName898 <danilkosta65@gmail.com>
2024-05-25 11:46:58 +0300

    Це лабораторна робота 4 (варіант 9)

---
+++ Lab_4 (АрхКомп).cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+int main()
+{
+    //Example:
+    //Source : C:\Users\Костя\Sou\
+    //Destination: C:\Users\Костя\Dest\
+    //File name: File.txt
+
+    string sourceDirectory, destinationDirectory, fileName;
+    cout << "Source:";
+    cin >> sourceDirectory;
+    cout << "Destination:";
+    cin >> destinationDirectory;
+    cout << "File name:";
+    cin >> fileName;
+
+    string moveCommand = "move " + sourceDirectory + fileName + " " + destinationDirectory;
+    cout << moveCommand;
+
+    int result = system(moveCommand.c_str());
+    if (result == 0) {
+        cout << "File moved successfully!" << std::endl;
+    }
+    else {
+        cout << "Error moving file." << std::endl;
+    }
+
+    return 0;
+}
